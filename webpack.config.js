20ce097155177be3b7ad71b74bc05c5587abe9b8
Tatyana Fomina <tayna-anita@mail.ru>
2020-06-13 21:40:18 +0300

    базовая настройка приложения

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+"use strict";
+
+const path = require("path");
+
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.tsx",
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"],
+  },
+  output: {
+    path: path.join(__dirname, "/dist"),
+    filename: "bundle.min.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      title: "User List App",
+    }),
+  ],
+};
