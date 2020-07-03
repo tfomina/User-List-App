c800cb69c079fe686df40da2561e30e8d2590ec3
Tatyana Fomina <tayna-anita@mail.ru>
2020-07-03 18:30:55 +0300

    Немного переделала конфиг вебпака и скрипт start в package.json

--- webpack.config.js
+++ webpack.config.js
@@ -6,13 +6,13 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
-  entry: "./src/index.tsx",
+  entry: path.resolve(__dirname, "src/index.tsx"),
   resolve: {
     extensions: [".ts", ".tsx", ".js"],
   },
   output: {
-    path: path.join(__dirname, "/dist"),
-    filename: "bundle.min.js",
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
   },
   module: {
     rules: [
